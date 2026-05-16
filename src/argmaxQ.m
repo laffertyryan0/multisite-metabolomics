fe09242bd6338ba1f41d1515edbd809d61ca1575
ryan <laffertyryan0@gmail.com>
2026-05-16 17:11:54 -0400

    changes to argmaxQ including fixing pdf calculation

--- src/argmaxQ.m
+++ src/argmaxQ.m
@@ -48,6 +48,13 @@ else
 
 end
 
+% DEBUG %%%%
+% for j = 1:r
+%     sigma_rho_tilde{j} = (1/mean(w))*getCovarianceOfCorrelations(...
+%                             vecLInverse(rho_est{j}),1/mean(w));
+% end
+%%%%%%%%%%%%
+
 
 iteration = 1;
 norm_grad = Inf;
@@ -65,36 +72,39 @@ pr_x_given_g_rel = zeros(L,r);
 % THIS LOOP IS SLOW: FIX 
 mu_Z_given_X = cell(L,r);
 for l=1:L
+    % The number of non-missing entries
     x_len = length(X{l});
+    % Calculate the mu and sig (permuted vectors) with appropriate
+    % partitions for the first mixing component. To be used for 
+    % denominators of relative probability ratios
     mu_est_j1 = P{l}*rho_est{1};
-    mu_X_est_j1 = mu_est_j1(1:x_len); 
+    mu_X_est_j1 = mu_est_j1(1:x_len); % Just the non-missing part
     sig_est_j1 = w(l)*P{l}*sigma_rho_est{1}*P{l}'; 
-    sig_XX_est_j1 = sig_est_j1(1:x_len,1:x_len);
-    sig_XX_est_j1_inv = inv(sig_XX_est_j1);
+    sig_XX_est_j1 = sig_est_j1(1:x_len,1:x_len); % Just the upper left block
     for j=1:r
         mu_est = P{l}*rho_est{j};
-        mu_X_est = mu_est(1:x_len); 
-        mu_Z_est = mu_est((x_len+1):end);
+        mu_X_est = mu_est(1:x_len); % Just the non-missing part
+        mu_Z_est = mu_est((x_len+1):end); % Just the missing part
         sig_est = w(l)*P{l}*sigma_rho_est{j}*P{l}'; 
-        sig_XX_est = sig_est(1:x_len,1:x_len);
-        sig_XZ_est = sig_est(1:x_len,(x_len+1):end);
+        sig_XX_est = sig_est(1:x_len,1:x_len); % Just the upper left block
+        sig_XZ_est = sig_est(1:x_len,(x_len+1):end); % Upper right block
         % Calculate conditional mean Z|X, which we will need for rho_tilde
         mu_Z_given_X{l,j} = ...
                 mu_Z_est + sig_XZ_est'*...
                 ((sig_XX_est)\(X{l}-mu_X_est)); %mu_{Z given X} or mu_{2|1}
         % Calculate relative pdf
-        pr_x_given_g_rel(l,j) = det(sig_XX_est_j1_inv*sig_XX_est)^(-1/2)*...
+        pr_x_given_g_rel(l,j) = det(sig_XX_est_j1\sig_XX_est)^(-1/2)*...
                                 exp(-(1/2)*(X{l}-mu_X_est)'*...
-                                (sig_XX_est)*...
-                                (X{l}-mu_X_est)+...
+                                ((sig_XX_est)\...
+                                (X{l}-mu_X_est))+...
                                 (1/2)*(X{l}-mu_X_est_j1)'*...
-                                (sig_XX_est_j1)*...
-                                (X{l}-mu_X_est_j1));
+                                ((sig_XX_est_j1)\...
+                                (X{l}-mu_X_est_j1)));
         %Calculate absolute pdf
         pr_x_given_g_abs(l,j) = (2*pi)^(-x_len/2)*det(sig_XX_est)^(-1/2)*...
                                 exp(-(1/2)*(X{l}-mu_X_est)'*...
-                                (sig_XX_est)*...
-                                (X{l}-mu_X_est));
+                                ((sig_XX_est)\...
+                                (X{l}-mu_X_est)));
     end
 end
 
@@ -106,21 +116,56 @@ end
 % Each column of pr_x_given_g is multiplied by corresponding scalar element
 % of alpha_est (jth) and then each row is divided by corresponding scalar
 % element of the L-vector sum(pr_x_given_g.*alpha_est,2); 
-pr_g_given_x = (pr_x_given_g_rel.*alpha_est)./...
-                sum(pr_x_given_g_rel.*alpha_est,2); 
+if ~sum(pr_x_given_g_rel>1e20,'all')>0
+    pr_g_given_x = (pr_x_given_g_rel.*alpha_est)./...
+                    sum(pr_x_given_g_rel.*alpha_est,2);
+else
+    pr_g_given_x = (pr_x_given_g_abs.*alpha_est)./...
+                    sum(pr_x_given_g_abs.*alpha_est,2); 
+end
+
 pr_g_given_x_lsum = sum(pr_g_given_x,1); % sum over l index
 pr_g_given_x_ljsum = L; % sum over j=1,..,r is just 1, so l,j sum is L
 
 % See expression for multinomial MLE 
 alpha_tilde = pr_g_given_x_lsum/pr_g_given_x_ljsum;
 
-% Step 3: Loop until max_iterations or ||gradient|| < tol
-while iteration <= max_iterations & norm_grad >= tol
+% for debugging, lets compute the actual estimates for a simple mixture
+% for j=1:r
+%     total = 0;
+%     for l=1:L
+%         total = total + pr_g_given_x(l,j)*X{l};
+%     end
+%     %rho_tilde{j} = total./pr_g_given_x_lsum(j);
+%     rho_tilde_calculated{j} = total./pr_g_given_x_lsum(j);
+% 
+% end
+
+% for debugging, lets compute the VARIANCE estimates for a simple mixture
+
+avg_num_samples = 1/mean(w);
+% for j=1:r
+%     total = 0;
+%     for l=1:L
+%         total = total + pr_g_given_x(l,j)*(X{l}-rho_tilde{j})^2;
+%     end
+%     sigma_rho_tilde{j} = avg_num_samples*total./pr_g_given_x_lsum(j);
+% end
+
+if 1
+end
+
+% rhos1 = [];
+% rhos2 = [];
+
+% 
+% % Step 3: Loop until max_iterations or ||gradient|| < tol
+while iteration <= max_iterations && norm_grad >= tol
     iteration = iteration + 1;
 
     % Step 3.1: Compute derivative of Q for remaining optimization (tilde) 
     %           variables (rho_tilde)
-    
+
                 gradient_rho_tilde = cell(1,r); % k(k-1)/2-vector for each 
                                                 % cell l
                 for i = 1:r
@@ -130,21 +175,30 @@ while iteration <= max_iterations & norm_grad >= tol
                         % l'th term in rho gradient sum 
                         total = total + ...
                            pr_g_given_x(l,i)*...
-                           P{l}'*inv(w(l)*P{l}*sigma_rho_tilde{i}*P{l}')...
-                           *([X{l} ; mu_Z_given_X{l,i}] - ...
-                           P{l}*rho_tilde{i});
+                           P{l}'*((w(l)*P{l}*sigma_rho_tilde{i}*P{l}')\...
+                           ([X{l} ; mu_Z_given_X{l,i}] - ...
+                           P{l}*rho_tilde{i}));
                     end
-                    gradient_rho_tilde{i} = total;
+                    denom_total = 0;
+                    for l = 1:L
+                        denom_total = denom_total + ...
+                           pr_g_given_x(l,i)*...
+                           P{l}'*inv(w(l)*P{l}*sigma_rho_tilde{i}*P{l}');
+                    end
+                    gradient_rho_tilde{i} = denom_total\total;
                 end
+
 
     % Step 3.2: Apply the gradient step to remaining optimization (tilde) 
     %           variables (rho_tilde)
 
                 for j=1:r
-                    rho_tilde{j} = rho_tilde{j} + ...
-                                   learning_rate*gradient_rho_tilde{j};
+                    increment = learning_rate*gradient_rho_tilde{j};
+                    rho_tilde{j} = rho_tilde{j} + increment;
                 end
 
+                % rhos1 = [rhos1 ; rho_tilde{1}];
+                % rhos2 = [rhos2 ; rho_tilde{2}];
 
     % Step 3.3: Apply the correlation projection to rho_tilde. Will need
     %           to convert to matrix form, apply projection, and convert
@@ -159,17 +213,35 @@ while iteration <= max_iterations & norm_grad >= tol
                         rho_tilde{j} = vecL(matrix_rho_tilde);
                     end
                 end
-                
-                norm_grad = 0;
+
+                norm_grad = 0; % maximum of all the gradient norms
                 for j=1:r
-                    norm_grad = norm_grad + norm(gradient_rho_tilde{j});
+                    norm_grad = max(norm_grad, norm(gradient_rho_tilde{j}));
                 end
+                norm_grad = norm_grad;
 
 end
+% figure
+% hold on
+% plot(1:length(rhos1),rhos1,'Color','blue');
+% plot(1:length(rhos1),(1:length(rhos1))*0 + rho_tilde_calculated{1},'Color','red')
+% title("rhos1")
+% figure
+% hold on
+% plot(1:length(rhos2),rhos2,'Color','blue');
+% plot(1:length(rhos1),(1:length(rhos1))*0 + rho_tilde_calculated{2},'Color','red')
+% title("rhos2")
 
 % Step 4: Return the new estimates
 alpha_est_new = alpha_tilde;
 rho_est_new = rho_tilde;
 sigma_rho_est_new = sigma_rho_tilde;
+
+disp("alpha_est")
+disp(alpha_est_new)
+disp("rho_est")
+disp(rho_est_new)
+disp("sigma_rho_est")
+disp(sigma_rho_est_new)
 
 end
